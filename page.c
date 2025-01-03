55712793230f1b0829563fd38e3b9e76cc1a78de
Amirsts <142572882+Amirsts@users.noreply.github.com>
2025-01-03 10:32:05 +0330

    added Fereydouns table

---
+++ page.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+
+	int i,x = 8,b,n;
+	printf("\n");
+	for (i = 8 ; i > 0 ; i--) {
+		printf(" *  *  *  *  *  *  *  *  %d\n", x);
+		x--;
+	}
+	printf(" A  B  C  D  E  F  G  H  \n");
+
+	printf("\n");
+
+	return 0;
+}
