c4ce8ce81a9805ce8b893151fb98dd887ab8a08c
Amirsts <142572882+Amirsts@users.noreply.github.com>
2025-01-03 21:30:28 +0330

    Update A02.c

--- A02.c
+++ A02.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
 
+
 int Valid(char satr, int satrAdad) {
     if (satr >= 'a' && satr <= 'h' && satrAdad >= 1 && satrAdad <= 8) {
-        return 1; 
-         // Valid input
+        return 1; // Valid input
     }
-    return 0;  
-         // Invalid input
+    return 0; // Invalid input
 }
 
-int main() {
+// Function to get valid 
+void GetValidMove() {
     char satrShoru, satrMaqsad;
     int shomareShoru, shomareMaqsad;
 
     while (1) {
         printf("Please enter the starting square (e.g., a1): ");
         scanf(" %c%d", &satrShoru, &shomareShoru);
-        satrShoru = tolower(satrShoru);  
-        
-        // Convert to lowercase
+        satrShoru = tolower(satrShoru);
 
-        printf("Please enter the destination  (e.g., b2): ");
+        printf("Please enter the destination square (e.g., b2): ");
         scanf(" %c%d", &satrMaqsad, &shomareMaqsad);
-        satrMaqsad = tolower(satrMaqsad);  
-        
-        // Convert to lowercase
+        satrMaqsad = tolower(satrMaqsad); 
 
         if (Valid(satrShoru, shomareShoru) && Valid(satrMaqsad, shomareMaqsad)) {
             printf("Valid input: %c%d to %c%d\n", satrShoru, shomareShoru, satrMaqsad, shomareMaqsad);
@@ -34,6 +30,12 @@ int main() {
             printf("Invalid input. Please try again.\n");
         }
     }
+
+    printf("Move registered from %c%d to %c%d.\n", satrShoru, shomareShoru, satrMaqsad, shomareMaqsad);
+}
+
+int main() {
+    GetValidMove();
 
     return 0;
 }
