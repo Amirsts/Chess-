c4354cde80b6e051e18ade78b6f69197174c5edd
MohammadSJalal <mohammadsaeedjalal.h@gmail.com>
2025-01-04 17:27:39 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -9,8 +9,9 @@ int rook(int i1 , int j1, int i2 , int j2);
 int queen(int i1 , int j1, int i2 , int j2);
 int King(int i1 , int j1, int i2 , int j2);
 void print_board();
-int convert(char j);
-int is_valid(int value , int state);
+int Valid(char satr, int satrAdad);
+int convertJ(char j);
+int convertI(char i);
 int command ();
 int ConvertToInt(char j);
 int main(void) {
@@ -20,11 +21,21 @@ int main(void) {
     return 0;
 }
 int command () {
-    char cmd1 [2];
-    char cmd2 [2];
-    scanf("%s", cmd1);
-    scanf("%s", cmd2);
+    printf("Please enter the motion : \n");
+    char cmd1[3], cmd2[3];
+    scanf("%2s", cmd1);
+    scanf("%2s", cmd2);
+    while (Valid(cmd1[0], ConvertToInt(cmd1[1])) == 0 || Valid(cmd2[0], ConvertToInt(cmd2[1])) == 0) {
+        printf("Sorry, your input is incorrect. Please enter again: \n");
+        scanf("%2s", cmd1);
+        scanf("%2s", cmd2);
+    }
 
+    int i1 = convertI(cmd1[1]);
+    int i2 = convertI(cmd2[1]);
+    int j1 = convertJ(cmd1[0]);
+    int j2 = convertJ(cmd2[0]);
+    printf("now i1 is %d\nj1 is %d\ni2 is %d\nj2 is %d", i1, j1, i2, j2);
 }
 int position(int i , int j) {
     /*
@@ -108,7 +119,7 @@ int pan(int i , int j, int i2 , int j2) {
 
     }
 }
-int convert(char j) {
+int convertJ(char j) {
     switch (j) {
         case 'a':
             return 0;
@@ -130,19 +141,10 @@ int convert(char j) {
             return -1;
     }
 }
+int convertI(char i) {
+    return 8-ConvertToInt(i);
+}
 
-int is_valid(int value , int state) {
-    if (state == 1) {
-        if (value <= 8 && value >= 0) {
-            return value;
-        }
-    }
-    while (convert(value) == state) {
-        printf("please enter a valid character : ");
-        scanf("%c", &value);
-    }
-    return value;
-};
 int ConvertToInt(char j) {
     switch (j) {
         case '0':
@@ -165,5 +167,17 @@ int ConvertToInt(char j) {
             return 8;
         case '9':
             return 9;
+        default:
+            return -1;
     }
+}
+int Valid(char satr, int satrAdad) {
+    if (satr >= 'a' && satr <= 'h' && satrAdad >= 1 && satrAdad <= 8) {
+        //printf("is Valid");
+        return 1;
+        // Valid input
+    }
+    //printf("is not valid");
+    return 0;
+    // Invalid input
 }
