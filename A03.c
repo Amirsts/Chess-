f0d37ddd9950b40f3aaedb51b8dfeaa72d826531
Amirsts <142572882+Amirsts@users.noreply.github.com>
2025-01-04 20:22:05 +0330

    checkturn

---
+++ A03.c
@@ -0,0 +1,22 @@
+
+
+checkturn(int i, int j)
+
+
+checkturn(int i , int j , int turn )
+
+if (position (i ,j) > 90 && turn == 0)
+{
+   return 1;
+}else if (position (i,j)<90 && turn == 1)
+
+return 1;
+
+else
+
+return 0;
+
+
+
+
+
