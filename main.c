92afb4ddaa34d2ce9d8e885279883c61c74556a4
MohammadSJalal <mohammadsaeedjalal.h@gmail.com>
2025-01-07 20:22:20 +0330

    v1.3
    new version of chess and play pan

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ int position(int i , int j);
 int move(int i1 , int j1, int i2 , int j2);
 void print_board();
 int command ();
-int sheckTurn(int i, int j, int turn_player);
+int checkTurn(int i1, int j1, int turn_player);
 void DoMove(int i1 , int j1, int i2 , int j2);
+void setCanMove();
+int getCommand(char bug);
 
 //function for pieces
 
-int pan(int i1 , int j1, int i2 , int j2);
+void pan(int i1 , int j1, int i2 , int j2);
 int knight(int i1 , int j1, int i2 , int j2);
 int bishop(int i1 , int j1, int i2 , int j2);
 int rook(int i1 , int j1, int i2 , int j2);
@@ -28,7 +30,7 @@ int limit_motion(int i, int j);
 int ConvertToInt(char j);
 
 //global variable and matrix
-
+char cmd1[3], cmd2[3];
 int turn_player = 1;                        //1 means white and 0 means black
 /*
 int location [8][8] = {
@@ -43,14 +45,19 @@ int location [8][8] = {
 };
 */
 int location[8][8];
-
+char canMove[8][8];
+//int i1 , j1 , i2 , j2;
 int main(void) {
     printf(
         "please attention to my note first if you want to exit program please enter ex\nyou must enter first position as the piece and second position as the where piece will go like \"e6 e7\"\n please attend to my warning\n");
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
+            canMove[i][j] = 0;
             if (i == 6) {
                 location[i][j] = 80;
+            }
+            else if (i == 1) {
+                location[i][j] = 112;
             }
             else {
                 location[i][j] = 46;
@@ -63,21 +70,29 @@ int main(void) {
 int command () {
     do{
         print_board();
-        printf("Please enter the motion : \n");
-        char cmd1[3], cmd2[3];
-        scanf("%2s", cmd1);
-        if (strcmp(cmd1, "ex") == 0)break;
-        scanf("%2s", cmd2);
+        if (turn_player)printf("Please enter the motion (white): \n");
+        else printf("Please enter the position (black): \n");
+        //char cmd1[3], cmd2[3];
+        //scanf("%2s", cmd1);
+        char exit = getCommand(0);
+        //if (strcmp(cmd1, "ex")==0) break;
+        if (exit || strcmp(cmd1,"ex")==0 ) break;
+        //scanf("%2s", cmd2);
         while (Valid(cmd1[0], ConvertToInt(cmd1[1])) == 0 || Valid(cmd2[0], ConvertToInt(cmd2[1])) == 0) {
-            printf("Sorry, your input is incorrect. Please enter again: \n");
-            scanf("%2s", cmd1);
-            scanf("%2s", cmd2);
+            getCommand(3);
+            //printf("Sorry, your input is incorrect. Please enter again: \n");
+            //scanf("%2s", cmd1);
+            //scanf("%2s", cmd2);
+        }
+        while (checkTurn(ConvertToInt(cmd1[1]) ,convertJ(cmd1[0]) ,turn_player)==0) {
+            getCommand(4);
         }
         int i1 = convertI(cmd1[1]);
         int i2 = convertI(cmd2[1]);
         int j1 = convertJ(cmd1[0]);
         int j2 = convertJ(cmd2[0]);
-        DoMove(i1, j1, i2, j2);
+        move(i1, i2, j1, j2);
+        //DoMove(i1, j1, i2, j2);
         turn_player++;
         turn_player = (turn_player == 2) ? 0 : 1 ;
         printf("now player is %d \n", turn_player);
@@ -100,12 +115,41 @@ int position(int i , int j) {
     */
     return location [i] [j];
 }
+void setCanMove() {
+    int i = 0, j = 0;
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            canMove[i][j] = 0;
+        }
+    }
+}
+int getCommand(char bug) {
+    switch (bug) {
+        case 1:
+            printf("Same position\n");
+            break;
+        case 2:
+            printf("Wrong position\n");
+            break;
+        case 3:
+            printf("Sorry, your input is incorrect. Please enter again: \n");
+            break;
+        case 4:
+            printf("Chosing the wrong nut\n");
+        default:
+            break;
+    }
+    scanf("%2s", cmd1);
+    if (strcmp(cmd1, "ex") == 0) return 1;
+    scanf("%2s", cmd2);
+    return 0;
+}
 void print_board() {
     int k=8;
     for (int i = 0; i < 8; i++) {
         printf("%d ", k--);
         for (int j = 0; j < 8; j++) {
-            printf("%c ",position(i,j));
+            printf("%c ",location[i][j]);
         }
         printf("\n");
     }
@@ -116,10 +160,14 @@ void DoMove(int i1 , int j1, int i2 , int j2) {
     location [i1][j1] = 46;
 }
 int move(int i1 , int j1 ,int i2 , int j2) {
-    int piece = position(i1,j1);
+    int piece = location[i1][j1];
     switch (piece) {
         case 80:
             pan(i1,j1,i2,j2);
+            if (canMove[i2][j2])DoMove(i1,j1,i2,j2);
+            else {
+                getCommand(3);
+            }
             break;
         case 112:
             pan(i1,j1,i2,j2);
@@ -159,13 +207,40 @@ int move(int i1 , int j1 ,int i2 , int j2) {
         }
 }
 
-int pan(int i , int j, int i2 , int j2) {
-    int motivate [4] = { 0, 0, 0 ,0}; //first digit is true if could move left. second is can move forward so on. last is for convert
+void pan(int i1 , int j1, int i2 , int j2) {
     if (turn_player) { //turn player 1 means piece white must move
-
+        if (i1 == 6) {
+            if (location [i1-2][j1] == 46 && location [i2-1][j1] == 46) {
+                canMove[i1-1][j1] = 1;
+                canMove[i1-2][j1] = 1;
+            }
+        }
+        if (location [i1-1][j1] == 46 ) {
+            canMove[i1-1][j1] = 1;
+        }
+        if (location [i1-1][j1-1] > 90) {
+            canMove[i1-1][j1-1] = 1;
+        }
+        if (location [i1-1][j1+1] > 90 ) {
+            canMove[i1-1][j1+1] = 1;
+        }
     }
     else {
-
+        if (i1 == 1) {
+            if (location [i1+2][j1] == 46 && location [i2+1][j1] == 46) {
+                canMove[i1+1][j1] = 1;
+                canMove[i1+2][j1] = 1;
+            }
+        }
+        if (location [i1+1][j1] == 46 ) {
+            canMove[i1+1][j1] = 1;
+        }
+        if (location [i1+1][j1-1] < 90 ) {
+            canMove[i1+1][j1-1] = 1;
+        }
+        if (location [i1+1][j1+1] > 90 ) {
+            canMove[i1+1][j1+1] = 1;
+        }
     }
 }
 int knight(int i1 , int j1, int i2 , int j2) {
