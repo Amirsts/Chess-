61d4146aaaf8ecc334dde5a75c2057a219495e61
Amir <142572882+Amirsts@users.noreply.github.com>
2025-01-13 23:30:19 +0330

    void (king)

--- king.c
+++ king.c
@@ -41,32 +41,32 @@ void knig(int i1 , int j1, int i2 , int j2);  {
     else {
 
         //upper third
-        if ((location [i1-1][j1] == 46) || (location [i1-1][j1] > 90) ) {
+        if ((location [i1-1][j1] == 46) || (location [i1-1][j1] < 90) ) {
             canMove[i1-1][j1] = 1;
         }
-        if ((location[i1-1][j1+1] == 46) || (location [i1-1][j1+1] > 90) ) {
+        if ((location[i1-1][j1+1] == 46) || (location [i1-1][j1+1] < 90) ) {
             canMove[i1-1][j1+1] = 1;
         }
-        if ((location [i1-1][j1-1]== 46) || (location [i1-1][j1-1] > 90) ) {
+        if ((location [i1-1][j1-1]== 46) || (location [i1-1][j1-1] < 90) ) {
             canMove[i1-1][j1-1] = 1;
         }
 
         //lelt and right
-        if ((location [i1][j1+1] == 46) || (location [i1][j1+1] > 90) ) {
+        if ((location [i1][j1+1] == 46) || (location [i1][j1+1] < 90) ) {
             canMove[i1][j1+1] = 1;
         }
-         if ((location [i1][j1-1] == 46) || (location [i1][j1-1] > 90) ) {
+         if ((location [i1][j1-1] == 46) || (location [i1][j1-1] < 90) ) {
             canMove[i1][j1-1] = 1;
         }
 
         //lower third
-        if ((location [i1+1][j1] == 46) || (location [i1+1][j1] > 90) ) {
+        if ((location [i1+1][j1] == 46) || (location [i1+1][j1] < 90) ) {
             canMove[i1+1][j1] = 1;
         }
-        if ((location[i1+1][j1+1] == 46) || (location [i1+1][j1+1] > 90) ) {
+        if ((location[i1+1][j1+1] == 46) || (location [i1+1][j1+1] < 90) ) {
             canMove[i1+1][j1+1] = 1;
         }
-        if ((location [i1+1][j1-1]== 46) || (location [i1+1][j1-1] > 90) ) {
+        if ((location [i1+1][j1-1]== 46) || (location [i1+1][j1-1] < 90) ) {
             canMove[i1+1][j1-1] = 1;
         }
        
